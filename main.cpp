bc05041d130cbc46996842dd909ae13fcc201e12
aditi1prakash <aditi1prakash@gmail.com>
2023-04-28 13:36:55 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*!
+ * \file    main.cpp
+ * \details Configure C++ project in vs-code environment with intellisense
+ * \author  Aditi Prakash
+ * \date    27-04-2023
+ */
+
+#include <iostream>
+using namespace std;
+
+/*!
+ * \brief   Main function
+ * \details 
+ * \return  int - return 0
+*/
+
+int main()
+{
+    cout << "Hello world!";
+
+    return 0;
+}
